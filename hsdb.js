18dd754f50fdc8e7773e680c28f534b66641f738
Hosein Khabbazian <hosenkh@gmail.com>
2014-08-23 15:21:39 +0430

    creat hsdatabase function

--- hsdb.js
+++ hsdb.js
@@ -1,3 +1,36 @@
+/**
+ * Hosein'sSQL Database
+ */
 (function(){
-  
+  var
+  /**
+   * main database function
+   */
+  hsdatabase = function(){
+    this.tables = {};
+  };
+
+  /**
+   * tables inside the database
+   */
+  hsdatebase.table = function(){
+    this.rows = [];
+  };
+
+  /**
+   * main selector function
+   * @param  {string} table the table which the data is selected from
+   * @param  {object} info  information for the selection
+   */
+  hsdatabase.prototype.select = function(table, info) {
+    // body...
+  };
+
+  /**
+   * the function to creat a new empty table in the database
+   * @param  {string} tablename table's name
+   */
+  hsdatabase.prototype.addtable = function(tablename) {
+    // body...
+  };
 })();
