ecd8e40194e790b47708391bfe879429ce41cd62
Hosein Khabbazian <hosenkh@gmail.com>
2014-08-23 16:40:28 +0430

    define typeDetector function

--- hsdb.js
+++ hsdb.js
@@ -1,14 +1,48 @@
 /**
  * Hosein'sSQL Database
  */
-(function(){
+(function(undef){
   var
   /**
    * main database function
    */
   hsdatabase = function(){
     this.tables = {};
-  };
+  },
+
+  typeDetector = function(a){
+    //difference between array and object is that array has a length but strings have length too
+    //if a variable was a date it is instanceof Date
+    //if a variable was a string it has a match function in its prototypes
+    //if a variable was a number typeof it is "number" but typeof NaN is "number" too
+    //typeof boolean and function are defined
+    //typeof null is object
+    if(isNaN(a) || a === null || a === undef || !isFinite(a)){
+      return "unusual";
+    }
+    if(typeof a =="string"){
+      return "string";
+    }
+    if(typeof a =="object" && a.length){
+      return "array";
+    }
+    if(typeof a =="object" && a instanceof Date){
+      return "date";
+    }
+    if(typeof a =="object" && !a.length && a !== null){
+      return "object";
+    }
+    if(typeof a =="boolean"){
+      return "boolean";
+    }
+    if(typeof a =="function"){
+      return "function";
+    }
+    if(typeof a =="number" && !isNaN(a)){
+      return "number";
+    }
+  }
+  ;
 
   /**
    * tables inside the database
@@ -30,7 +64,7 @@
    * the function to creat a new empty table in the database
    * @param  {string} tablename table's name
    */
-  hsdatabase.prototype.addtable = function(tablename) {
+  hsdatabase.prototype.addTable = function(tablename) {
     // body...
   };
 })();
