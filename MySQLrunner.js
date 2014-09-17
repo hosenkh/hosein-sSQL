f04604a79bb07b209c0cac01426a6d4d7b6885fc
Hosein Khabbazian <hosenkh@gmail.com>
2014-09-17 10:14:56 +0330

    new functions in MySQL runner

--- MySQLrunner.js
+++ MySQLrunner.js
@@ -2,15 +2,25 @@
   var
   mysql = require('mysql'),
   readline = require('readline'),
-  readLine = readline.createInterface({input: process.stdin, output: process.stdout}),
-  liveDB,
-  liveTable,
+  liveDB = null,
+  liveTable = null,
   liveUser,
+  liveArray = [],
+  stringify = function(a){
+    if (a === null) {
+      return 'null';
+    } else {
+    return a.toString();
+    }
+  },
+  spaces = ['',' ','  ','   ','    ','     ','      ','       ','        ','         ','          ','           ','            ','             ','              ','               ','                ','                 ','                  ','                   '],
+  status = 'loggedOff',
   form = function(formName, levelArray, retFunc){
     var
     levelIndex = 0,
     answers = [];
-    readLine.write(formName+'\n');
+    console.log(formName);
+    readLine = readline.createInterface({input: process.stdin, output: process.stdout});
     readLine.setPrompt(levelArray[levelIndex]);
     readLine.prompt();
     readLine.on('line', function(line){
@@ -22,6 +32,7 @@
       } else {
         answers[levelIndex]=line;
         readLine.close();
+        return;
       }
     }).on('close', function(){
       retFunc(answers);
@@ -32,25 +43,281 @@
     form('login', levelArray, function(answers){
       connection = mysql.createConnection({user: answers[0], password: answers[1]});
       connection.connect();
-      connection.end();
+      status = 'loggedOn';
+      init();
     });
-
-
-
-    
-    
+  },
+  showDBs = function(){
+    connection.query('SHOW DATABASES', function(err, result){
+      if (err) console.error('Unable to show databases.');
+      liveArray = [];
+      liveDB = null;
+      liveTable = null;
+      for(var i in result){
+        liveArray.push(result[i].Database);
+      }
+      console.log('\nDatabases:\n');
+      console.log(liveArray);
+      status = 'showDBsDone';
+      init();
+    });
+  },
+  showDBsForm = function(){
+    var
+    formName = 'Which one do you want?\na)add a database\nb)delete a database\nc)use a database\nd)exit\ne)want more or less...',
+    levelArray = ['Write the letter only: '],
+    retFunc = function(answers){
+      switch (answers[0]){
+        case 'a':
+          status = 'wantsToAddDB';
+          init();
+        break;
+        case 'b':
+          status = 'wantsToDeleteDB';
+          init();
+        break;
+        case 'c':
+          status = 'wantsToUseDB';
+          init();
+        break;
+        case 'd':
+          status = 'wantsToExit';
+          init();
+        break;
+        case 'e':
+          status = 'wantMore';
+          init();
+        break;
+        default:
+          status = 'showDBsDone';
+          init();
+        break;
+      }
+    };
+    form(formName, levelArray, retFunc);
   },
   logout = function(){
     connection.end();
   },
+  addDB = function(){
+    var
+    formName = 'adding a new database',
+    levelArray = ['name of the database: ',"admin's username, not 'admin' itself!: ", "admin's password: ", 'username for public user, not "public" itself!: ', 'password of the public user: '],
+    retFunc = function(answers){
+      connection.query('CREATE DATABASE '+answers[0]);
+      connection.query('CREATE USER "'+answers[1]+'"@"localhost" IDENTIFIED BY "'+answers[2]+'"');
+      connection.query('GRANT ALL PRIVILEGES ON '+answers[0]+'.* TO "'+answers[1]+'"@"localhost"');
+      connection.query('FLUSH PRIVILEGES');
+      console.log(answers);
+      connection.query('CREATE USER "'+answers[3]+'"@"localhost" IDENTIFIED BY "'+answers[4]+'"');
+      connection.query('GRANT DELETE ON '+answers[0]+'.* TO "'+answers[3]+'"@"localhost"');
+      connection.query('GRANT INSERT ON '+answers[0]+'.* TO "'+answers[3]+'"@"localhost"');
+      connection.query('GRANT SELECT ON '+answers[0]+'.* TO "'+answers[3]+'"@"localhost"');
+      connection.query('FLUSH PRIVILEGES');
+      status = 'loggedOn';
+      init();
+    };
+    form(formName,levelArray,retFunc);
+  },
+  deleteDB = function(){
+    var
+    formName = 'delete a database, if you are logged in as the admin of this database, you cannot delete it.',
+    levelArray = ['name of the database: ', 'name of admin user: ', 'name of public user: ', 'Are you sure you want to delete the whole database? (yes, any other words mean NO): '],
+    retFunc = function(answers){
+      if(answers[3]=='yes'){
+        if (liveArray.indexOf(answers[0])!== -1){
+          connection.query('DROP DATABASE '+answers[0]);
+          connection.query('DROP USER "'+answers[1]+'"@"localhost"');
+          connection.query('DROP USER "'+answers[2]+'"@"localhost"');
+        }
+      } else {
+        console.log('There is no database with the specified name.');
+      }
+      status = 'loggedOn';
+      init();
+    };
+    form(formName, levelArray, retFunc);
+  },
+  useDB = function(){
+    var
+    formName = 'use a database',
+    levelArray = ['name of the database: '],
+    retFunc = function(answers){
+      connection.query('USE '+answers[0]);
+      liveDB = answers[0];
+      status = 'usingDB';
+      init();
+    };
+    form(formName, levelArray, retFunc);
+  },
+  showTables = function(){
+    connection.query('SHOW TABLES', function(err, result){
+      if (err) {
+        console.error('Unable to show databases.');
+        return;
+      }
+      liveTable = null;
+      liveArray = [];
+      if(liveDB){
+        for(var i in result){
+          liveArray.push(result[i]['Tables_in_'+liveDB]);
+        }
+      }
+      console.log('\ntables in '+liveDB+':\n');
+      console.log(liveArray);
+      status = 'tablesShown';
+      init();
+    });
+  },
+  tablesShown = function(){
+    var
+    formName = 'which one do you want?\na)add a table\nb)delete a table\nc)use a table\nd)want more or less...\ne)back to list of databases\nf)exit',
+    levelArray = ['Write the letter only: '],
+    retFunc = function(answers){
+      switch (answers[0]){
+        case 'a':
+          status = 'wantsToAddTable';
+          init();
+        break;
+        case 'b':
+          status = 'wantsToDeleteTable';
+          init();
+        break;
+        case 'c':
+          status = 'wantsToUseTable';
+          init();
+        break;
+        case 'd':
+          status = 'wantMore';
+          init();
+        break;
+        case 'e':
+          status = 'loggedOn';
+          init();
+        break;
+        case 'f':
+          status = 'wantsToExit';
+          init();
+        break;
+        default:
+          status = 'usingDB';
+          init();
+        break;
+      }
+    };
+    form(formName,levelArray,retFunc);
+  },
+  wantMore = function(){
+    var
+    formName = 'want more or less? hit "close" and use "mysql -u [your username] -p" and use professional codes.',
+    levelArray = ['> '],
+    retFunc = function(answers){
+      if(answers[0] == 'close'){
+        status = 'wantsToExit';
+        init();
+      } else {
+        status = 'loggedOn';
+        init();
+      }
+    };
+    form(formName, levelArray, retFunc);
+  },
   addTable = function(){
-    connection.query('CREATE TABLE '+tableName+' (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT)',errorLogger);
-    connection.end(errorLogger);
+    connection.query('CREATE TABLE '+tableName+' (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT)');
   },
   deleteTable = function(db,tableName){
-    dbConnector(db);
-    connection.query('DROP TABLE '+tableName);
-    connection.end(errorLogger);
+    var
+    formName = 'delete table',
+    levelArray = ['name of the table you want to delete: ', 'Are you sure you want to delete the table named above? (yes, any other words mean NO): '],
+    retFunc = function(answers){
+      if (answers[1]=== 'yes'){
+        connection.query('DROP TABLE '+answers[0], function(err){
+          if (err) console.log('cannot delete table '+answers[0]);
+        });
+      }
+        status = 'usingDB';
+        init();
+    };
+    form(formName,levelArray,retFunc);
+  },
+  useTable = function(){
+    var
+    formName = 'use a table',
+    levelArray = ['name of the table you want to use: '],
+    retFunc = function(answers){
+      liveTable = answers[0];
+      status = 'usingTable';
+      init();
+    };
+    form(formName,levelArray,retFunc);
+  },
+  showFields = function(){
+    connection.query('DESCRIBE '+liveTable, function(err, result){
+      if (err) {
+        console.error('Unable to show the fields of '+liveTable+'.');
+        return;
+      }
+      liveArray = [];
+      if(liveTable){
+        liveArray = result;
+      }
+      console.log('\nfields in '+liveTable+':\n');
+      console.log('|Field               |Type       |Null|Key|Default|Extra          |');
+      for (var i in liveArray){
+        console.log('|'+liveArray[i].Field+spaces[20-liveArray[i].Field.length]+'|'+liveArray[i].Type+spaces[11-stringify(liveArray[i].Type).length]+'|'+liveArray[i].Null+spaces[4-stringify(liveArray[i].Null).length]+'|'+liveArray[i].Key+spaces[3-stringify(liveArray[i].Key).length]+'|'+liveArray[i].Default+spaces[7-stringify(liveArray[i].Default).length]+'|'+liveArray[i].Extra+spaces[15-stringify(liveArray[i].Extra).length]+'|');
+      }
+      status = 'fieldsShown';
+      init();
+    });
+  },
+  init = function(){
+    switch (status){
+      case 'loggedOff':
+        login();
+      break;
+      case 'loggedOn':
+        showDBs();
+      break;
+      case 'showDBsDone':
+        showDBsForm();
+      break;
+      case 'wantsToDeleteDB':
+        deleteDB();
+      break;
+      case 'wantsToAddDB':
+        addDB();
+      break;
+      case 'wantsToUseDB':
+        useDB();
+      break;
+      case 'wantsToExit':
+        logout();
+      break;
+      case 'usingDB':
+        showTables();
+      break;
+      case 'wantMore':
+        wantMore();
+      break;
+      case 'tablesShown':
+        tablesShown();
+      break;
+      case 'wantsToAddTable':
+        addTable();
+      break;
+      case 'wantsToDeleteTable':
+        deleteTable();
+      break;
+      case 'wantsToUseTable':
+        useTable();
+      break;
+      case 'usingTable':
+        showFields();
+      break;
+      case 'fieldsShown':
+        logout();
+      break;
+    }
   };
 
 
@@ -125,5 +392,5 @@
     //  SET('value 1', 'value 2', ...) //zero or more values from defined values (up to 64 values) 
   //   connection.end(errorLogger);
   // },
-  return {init: login};
+  return {init: init};
 })().init();
--- databaseInitializer.js
+++ databaseInitializer.js
@@ -5,37 +5,16 @@
   database = 'testDB',
   adminPass = 'qqa',
   publicpass = 'qqa',
-  connection = mysql.createConnection({user: 'root', password: ''}),
-  readLine = readline.createInterface({input: process.stdin, output: process.stdout})
+  connection = mysql.createConnection({user: 'root', password: ''})
   ;
   connection.connect();
-  /**
-   * create the database
-   */
-  connection.query('CREATE DATABASE '+database);
-  /**
-   * create admin user
-   */
-  connection.query('CREATE USER "admin"@"localhost" IDENTIFIED BY "'+adminPass+'"');
-  connection.query('GRANT ALL PRIVILEGES ON '+database+'.* TO "admin"@"localhost"');
-  connection.query('FLUSH PRIVILEGES');
-  /**
-   * create public user
-   */
-  connection.query('CREATE USER "public"@"localhost" IDENTIFIED BY "'+publicpass+'"');
-  connection.query('GRANT DELETE ON '+database+'.* TO "public"@"localhost"');
-  connection.query('GRANT INSERT ON '+database+'.* TO "public"@"localhost"');
-  connection.query('GRANT SELECT ON '+database+'.* TO "public"@"localhost"');
-  connection.query('FLUSH PRIVILEGES');
   /**
    * use the database
    */
   connection.query('USE '+database);
-
-  connection.query('CREATE TABLE signUp (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, firstName VARCHAR(15), lastName VARCHAR(15), userName VARCHAR(10) NOT NULL, password VARCHAR(18) NOT NULL, email VARCHAR(40), tel SMALLINT(13))');
-  readLine.question('sdsdfsdf', function(answer){
-    console.log(answer);
-    readLine.close();
-  });
+  /**
+   * create users table
+   */
+  connection.query('CREATE TABLE users (userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT, firstName VARCHAR(15), lastName VARCHAR(15), userName VARCHAR(10) NOT NULL, password VARCHAR(18) NOT NULL, email VARCHAR(40), tel SMALLINT(13), isActive BOOL)');
   connection.end();
 })();
